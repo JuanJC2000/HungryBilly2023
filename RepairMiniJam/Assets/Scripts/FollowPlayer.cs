7f9fa3af175b11adecd7b9bce251f2eb364d3134
jj246324 <jj246324@falmouth.ac.uk>
2023-05-01 15:35:47 +0100

    Added more offsets to camera

--- RepairMiniJam/Assets/Scripts/FollowPlayer.cs
+++ RepairMiniJam/Assets/Scripts/FollowPlayer.cs
@@ -8,17 +8,19 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset = new Vector3(0, 5, -7); // set new private vector 3 offset to make code easily editable for future offset of camera if need be
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
     //using late update for smoothness of camera
-    // Update is called once per frame
+    
     void LateUpdate()
     {
-        //offset the camewra behind the player by adding to the players position
+        //offset the camera behind the player by adding to the players position
+     
+        transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.position = player.transform.position + offset;
     }
 }
